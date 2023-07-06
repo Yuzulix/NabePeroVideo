7029dc7ecf48b25132677acf2d8a62a34a24448b
Smile <smailbestybay@gmail.com>
2023-07-05 23:00:30 -0700

    start contact page

--- yuzulix/src/App.jsx
+++ yuzulix/src/App.jsx
@@ -5,17 +5,19 @@ import About from "./pages/About";
 import SimpleAppBar from "./components/AppBar";
 import Footer from "./components/Footer";
 import { Container, Box } from "@mui/material";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <>
       <Box sx={{ minHeight: "100vh", position: "relative" }}>
-        <Box sx={{paddingBottom: '5rem'}}>
+        <Box sx={{ paddingBottom: "5rem" }}>
           <SimpleAppBar />
           <Container sx={{ marginTop: 10 }}>
             <Routes>
               <Route path="/Home" element={<Home />} />
               <Route path="/About" element={<About />} />
+              <Route path="/Contact" element={<Contact />} />
             </Routes>
           </Container>
         </Box>
---
+++ yuzulix/src/pages/Contact.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+const Contact = () => {
+
+  return (
+    <Box>
+      Name
+      Email 
+      Message
+    </Box>
+  )
+};
+
+export default Contact;
