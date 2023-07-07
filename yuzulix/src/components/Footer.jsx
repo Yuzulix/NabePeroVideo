75077876bfe5af401a97d6bad0ce0bee2812b038
Smile <smailbestybay@gmail.com>
2023-07-06 23:28:48 -0700

    add links to footer

--- yuzulix/src/components/Footer.jsx
+++ yuzulix/src/components/Footer.jsx
@@ -1,28 +1,39 @@
-import { Box, Typography } from "@mui/material";
-
+import { Box } from "@mui/material";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import logo from "../assets/nabepero-logo.png";
+import MuiLink from "@mui/material/Link";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <Box component="footer" sx={{position: 'absolute', bottom: '0', height: '2rem'}}>
+    <Box
+      component="footer"
+      sx={{ position: "absolute", bottom: "0", height: "2rem" }}
+    >
       <Box
         sx={{
           display: "flex",
-          backgroundColor: "secondary.main",
-          padding: 3,
+          justifyContent: "center",
+          gap: 12,
+          borderTop: "1px solid",
+          borderColor: "secondary.main",
+          padding: 4,
           width: "100vw",
           left: "calc(-50vw + 50%)",
         }}
       >
-        <Box sx={{ flexGrow: 2 }}>
-          <Typography component="p">LEFT</Typography>
-          <Typography component="p">LEFT</Typography>
-          <Typography component="p">LEFT</Typography>
-          <Typography component="p">LEFT</Typography>
-          <Typography component="p">LEFT</Typography>
+        <Box>
+          <Link style={{ textDecoration: "none" }} to={"/Home"}>
+            <img src={logo} alt="logo" width={200} className="classes.logo" />
+          </Link>
         </Box>
-        <Box sx={{ flexGrow: 3 }}>
-          <Typography component="p">RIGHT</Typography>
-          <Typography component="p">RIGHT</Typography>
-          <Typography component="p">RIGHT</Typography>
+        <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
+          <MuiLink href="https://www.instagram.com/nabepero_ph/">
+            <InstagramIcon />
+          </MuiLink>
+          <MuiLink href="https://www.facebook.com/profile.php?id=100063795950849">
+            <FacebookIcon />
+          </MuiLink>
         </Box>
       </Box>
     </Box>
